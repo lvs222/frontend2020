f528f3a2bc20c8e0d907f7bd32de3c2fa29259ff
lvs222 <51123194+lvs222@users.noreply.github.com>
2021-01-12 20:24:32 +0100

    20:30 update

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,1 +1,9 @@
-// JavaScript Document
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', animateHamburger);
+
+function animateHamburger() {
+    hamburger.classList.toggle('active');
+    navigation.classList.toggle('active');
+};
